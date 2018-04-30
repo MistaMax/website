806f96ed67f4861b6eff1c1484cd2db4915f7593
Mista Max <xbox1ftw@outlook.com>
2018-04-30 14:58:41 -0500

    added test files to gds project

---
+++ public_html/gds-project/h.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="main.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+
+    <title>GDS</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body id="body-over">
+<nav class="site-header sticky-top py-1" id="menu-table">
+    <div class="container d-flex flex-column flex-md-row justify-content-between">
+        <a class="py-2" href="nav.html">
+            <img src="cart.gif" id="logo-badge" width="60" height="60">
+        </a>
+        <a class="py-2 d-none d-md-inline-block display-4 align-bottom" id="menu-item" href="index.html">Home</a>
+        <a class="py-2 d-none d-md-inline-block display-4 align-bottom" id="menu-item" href="login.php">Login</a>
+    </div>
+</nav>
---
+++ public_html/gds-project/i.php
@@ -0,0 +1,20 @@
+<?php
+	include_once 'h.php';
+?>
+
+<div class="container-fluid">
+<div class="row">
+    <div class="col">
+        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
+            <div class="col-md-5 p-lg-5 mx-auto my-5">
+                <h1 class="display-4 font-weight-normal">Grocery Delivery System</h1>
+                <p class="lead font-weight-normal">We supply people with groceries on demand</p>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+<?php
+	include_once 'f.php';
+?>
