d1d7873daf76c96e1bea1884a1f678d0e881718f
Mista Max <xbox1ftw@outlook.com>
2018-04-30 13:20:13 -0500

    sperated db file

--- public_html/gds-project/includes/dbh.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "loginsystem";
-
-$connection = mysqli_connect($dbServername,$dbUsername, $dbPassword, $dbName);
