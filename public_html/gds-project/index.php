6f5bebfd84769ef38ae2d61783763ee372f02936
Mista Max <xbox1ftw@outlook.com>
2018-05-02 20:28:37 -0500

    making the table less opaque

--- public_html/gds-project/index.php
+++ public_html/gds-project/index.php
@@ -16,8 +16,8 @@
 				</div>
 				</div>
 				<div class="col">
-				<div class="overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
-                    <div class="col-md-5 p-lg-5 mx-auto my-5">
+				<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
+                    <div class="col-md-5 p-lg-5 mx-auto my-5 b50">
 						<p class="lead font-weight-normal">
 				<div class="dropdown">
 				<button class="dropbtn">Take Action!</button>
