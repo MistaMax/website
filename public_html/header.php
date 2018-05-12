cf2a29b508ac454213c76856d6150ff0fbf78f87
MistaMax <mmc_the_ace@yahoo.com>
2018-05-12 13:03:42 -0500

    added base index.php and etc for my website

---
+++ public_html/header.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="main.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Max Crookshanks</title>
+	<!--<link rel="stylesheet" type="text/css" href="style.css">-->
+</head>
+<body id="body-over">
+<nav class="site-header sticky-top py-1" id="menu-table">
+    <div class="container d-flex flex-column flex-md-row justify-content-between">
+        <!--<a class="py-2" href="nav.html">
+            <img src="cart.gif" id="logo-badge" width="60" height="60">
+        </a>-->
+        <a class="py-2 d-none d-md-inline-block display-4 align-bottom" id="menu-item" href="index.php">Home</a>
+    </div>
+</nav>
---
+++ public_html/index.php
@@ -0,0 +1,68 @@
+<?php
+	include_once 'header.php';
+?>
+<div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
+                    <div class="col-md-5 p-lg-5 mx-auto my-5">
+                        <h1 class="display-4 font-weight-normal">Max Crookshanks</h1>
+                        <p class="lead font-weight-normal">This website has everything you need to know about me</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
+                    <div class="col-md-5 p-lg-5 mx-auto my-5">
+                        <h1 class="display-4 font-weight-normal">Summary</h1>
+                        <p class="lead font-weight-normal">Insert Summary</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
+                    <div class="col-md-5 p-lg-5 mx-auto my-5">
+                        <h1 class="display-4 font-weight-normal">Hobbies</h1>
+                        <p class="lead font-weight-normal">Insert Hobbies</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
+                    <div class="col-md-5 p-lg-5 mx-auto my-5">
+                        <h1 class="display-4 font-weight-normal">Qualifications and Skills</h1>
+                        <p class="lead font-weight-normal">Insert Qualifications Here</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
+                    <div class="col-md-5 p-lg-5 mx-auto my-5">
+                        <h1 class="display-4 font-weight-normal">Education</h1>
+                        <p class="lead font-weight-normal">University of Texas at San Antonio</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
+                    <div class="col-md-5 p-lg-5 mx-auto my-5">
+                        <h1 class="display-4 font-weight-normal">Work Experience</h1>
+                        <p class="lead font-weight-normal">Insert Work Experience</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+<?php
+	include_once 'footer.php';
+?>
