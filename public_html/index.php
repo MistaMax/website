d8ff1003fe9779381f19ea9fe3e68cafe2cacef6
MistaMax <mmc_the_ace@yahoo.com>
2018-05-14 15:33:00 +0000

    Added a bit more info about myself

--- public_html/index.php
+++ public_html/index.php
@@ -17,7 +17,7 @@
                 <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
                     <div class="col-md-5 p-lg-5 mx-auto my-5">
                         <h1 class="display-4 font-weight-normal">Summary</h1>
-                        <p class="lead font-weight-normal">Insert Summary</p>
+                        <p class="lead font-weight-normal">Im max</p>
                     </div>
                 </div>
             </div>
