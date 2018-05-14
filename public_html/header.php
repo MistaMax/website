92748bec2fec03790b8ead262bc019414e46173d
MistaMax <mmc_the_ace@yahoo.com>
2018-05-14 11:26:49 -0500

    added a few parts of the website for organizational purposes

--- public_html/header.php
+++ public_html/header.php
@@ -10,11 +10,11 @@ session_start();
 	<!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="style/main.css">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
         crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style/style.css">
     <title>Max Crookshanks</title>
 	<!--<link rel="stylesheet" type="text/css" href="style.css">-->
 </head>
