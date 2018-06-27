3f1d3e2634ec801bacd380deff16d059dac6f663
MistaMax <mmc_the_ace@yahoo.com>
2018-06-27 17:26:56 -0500

    Fixed logo

--- public_html/roadrunner-gaming.1/header.php
+++ public_html/roadrunner-gaming.1/header.php
@@ -20,7 +20,7 @@ session_start();
 </head>
 <body id="body-over">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-<a class="navbar-brand" href="index.php">Roadrunner Gaming</a>
+<a class="navbar-brand" href="index.php"><img src="RRGaming_Logo.png" id="logo-badge" width="50" height="50">Roadrunner Gaming</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
     aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -28,10 +28,6 @@ session_start();
 
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="index.php"><img src="RRGaming_Logo.png" id="logo-badge" width="50" height="50">
-            </a>
-        </li>
         <li class="nav-item active">
             <a class="nav-link" href="index.php">Home
                 <span class="sr-only">(current)</span>
