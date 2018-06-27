5df8f5bc704559706fb59e91d269d48023b975d6
MistaMax <mmc_the_ace@yahoo.com>
2018-06-27 17:25:17 -0500

    Added logo to menu

--- public_html/roadrunner-gaming.1/header.php
+++ public_html/roadrunner-gaming.1/header.php
@@ -29,6 +29,10 @@ session_start();
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
+            <a class="nav-link" href="index.php"><img src="RRGaming_Logo.png" id="logo-badge" width="50" height="50">
+            </a>
+        </li>
+        <li class="nav-item active">
             <a class="nav-link" href="index.php">Home
                 <span class="sr-only">(current)</span>
             </a>
