e806bc8c2c7c74ac9cbbff0485a86a7a6fee9089
Mista Max <mmctheace@gmail.com>
2018-09-15 14:53:35 -0500

    updating chevron edit

--- public_html/chevron/index.php
+++ public_html/chevron/index.php
@@ -1,7 +1,7 @@
 <?php
 	include_once 'header.php';
 ?>
-<div class="container-fluid">
+<div class="central-container">
         <div class="row">
             <div class="col">
                 <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
