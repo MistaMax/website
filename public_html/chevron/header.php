f610b6cd0d213e6da2ca69a34b3582f5f881691d
Mista Max <mmctheace@gmail.com>
2018-09-15 14:27:18 -0500

    updating chevron edit

--- public_html/chevron/header.php
+++ public_html/chevron/header.php
@@ -21,7 +21,7 @@ session_start();
 <body id="body-over">
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
 <a class="navbar-brand" href="index.php"><img src="chevron.png" id="logo-badge" width="50" height="50">Chevron Work Orders</a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
     aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
 </button>
