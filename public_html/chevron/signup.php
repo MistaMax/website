c1c9096549c937582e55aad546fc65064fa41975
Mista Max <mmctheace@gmail.com>
2018-09-15 14:59:35 -0500

    updating chevron edit

--- public_html/chevron/signup.php
+++ public_html/chevron/signup.php
@@ -6,6 +6,7 @@
             <div class="col">
                 <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
 					<div class="col-md-12 p-lg-5 mx-auto my-5">
+					<h1 class="display-4 font-weight-normal">Sign Up</h1>
 					<form class="signup-form" action="includes/signup.inc.php" method="POST">
 			<input type="text" name="uid" placeholder="Username">
 			<input type="password" name="pwd" placeholder="Password">
