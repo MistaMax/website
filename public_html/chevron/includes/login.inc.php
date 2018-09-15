f72a279088ef91daa09f8e9d7baa873654affc92
Mista Max <mmctheace@gmail.com>
2018-09-15 15:19:25 -0500

    updating chevron edit

--- public_html/chevron/includes/login.inc.php
+++ public_html/chevron/includes/login.inc.php
@@ -11,32 +11,32 @@ if (isset($_POST['submit'])) {
 	
 	//Check if empty
 	if (empty($uid) || empty($pwd)){
-		header("Location: ../index.php?login=error1");
+		header("Location: ../signin.php?login=error1");
 		exit();
 	}else {
 		$sql = "SELECT * FROM Login WHERE szUsername='$uid'";
 		$result = mysqli_query($connection, $sql);
 		$resultCheck = mysqli_num_rows($result);
 		if ($resultCheck < 1){
-			header("Location: ../index.php?login=error2");
+			header("Location: ../signin.php?login=error2");
 			exit();
 		}else {
 			if ($row = mysqli_fetch_assoc($result)){
 				//dehashing
 				$hashedPwdCheck = password_verify($pwd, $row['szPassword']);
 				if ($hashedPwdCheck == false){
-					header("Location: ../index.php?login=error3");
+					header("Location: ../signin.php?login=error3");
 					exit();
 				}elseif ($hashedPwdCheck == true){
 					//login
 					$_SESSION['u_id'] = $row['szUsername'];
-					header("Location: ../index.php?login=success");
+					header("Location: ../signin.php?login=success");
 					exit();
 				}
 			}
 		}
 	}
 }else{
-	header("Location: ../index.php?login=error");
+	header("Location: ../signin.php?login=error");
 	exit();
 }
