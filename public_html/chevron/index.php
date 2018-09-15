7b4fb0276f206b605b1a1e71be1dce8a81110b40
Mista Max <mmctheace@gmail.com>
2018-09-15 14:55:45 -0500

    updating chevron edit

--- public_html/chevron/index.php
+++ public_html/chevron/index.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col">
                 <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
-					<div class="col-md-12 p-lg-12 mx-auto my-12">
+					<div class="col-md-12 p-lg-5 mx-auto my-5">
 						<h1 class="display-4 font-weight-normal">Chevron Edit</h1>
 						<p class="lead font-weight-normal">Editing</p>
 					</div>
