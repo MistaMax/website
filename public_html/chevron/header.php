d64cd1413d309018876b48b1baf2571f6b4d8e1c
Mista Max <mmctheace@gmail.com>
2018-09-15 11:21:24 -0500

    added base chevron code

---
+++ public_html/chevron/header.php
@@ -0,0 +1,41 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="main.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Chevron Work Order Organizer</title>
+	<!--<link rel="stylesheet" type="text/css" href="style.css">-->
+</head>
+<body id="body-over">
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="index.php"><img src="RRGaming_Logo.png" id="logo-badge" width="50" height="50">Chevron Work Orders</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
+    aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+        <a class="py-2 d-none d-md-inline-block display-4 align-bottom" id="menu-item" href="index.php">Home</a>
+    </li>
+    <li class="nav-item active">
+        <a class="py-2 d-none d-md-inline-block display-4 align-bottom" id="menu-item" href="signIn.php">Sign In</a>
+    </li>
+    <li class="nav-item active">
+        <a class="py-2 d-none d-md-inline-block display-4 align-bottom" id="menu-item" href="signup.php">Sign Up</a>
+    </li>
+    </ul>
+</div>
+</nav>
---
+++ public_html/chevron/index.php
@@ -0,0 +1,8 @@
+<?php
+	include_once 'header.php';
+?>
+<?php
+?>
+<?php
+	include_once 'footer.php';
+?>
