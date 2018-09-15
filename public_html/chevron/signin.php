8ceb95b2be341ba5e0103cfc633b56677bcd686f
Mista Max <mmctheace@gmail.com>
2018-09-15 14:57:51 -0500

    updating chevron edit

--- public_html/chevron/signin.php
+++ public_html/chevron/signin.php
@@ -1,17 +1,21 @@
 <?php
 	include_once 'header.php';
 ?>
-
-<section class="main-container">
-	<div class="main-wrapper">
-		<h2>Sign Up</h2>
-		<form class="signup-form" action="includes/signup.inc.php" method="POST">
+<div class="central-container">
+        <div class="row">
+            <div class="col">
+                <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
+					<div class="col-md-12 p-lg-5 mx-auto my-5">
+                    <form class="signup-form" action="includes/signup.inc.php" method="POST">
 			<input type="text" name="uid" placeholder="Username">
 			<input type="password" name="pwd" placeholder="Password">
 			<button type="submit" name="submit">Sign In</button>
 		</form>
-	</div>
-</section>
+					</div>
+				</div>
+			</div>
+		</div>
+</div>
 
 <?php
 	include_once 'footer.php';
