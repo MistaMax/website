b7afd52e549493a9cf94a794cb43f322db6ca213
Mista Max <mmctheace@gmail.com>
2018-09-15 14:24:18 -0500

    added base chevron code

--- public_html/chevron/index.php
+++ public_html/chevron/index.php
@@ -1,8 +1,19 @@
 <?php
 	include_once 'header.php';
 ?>
-<?php
-?>
+<div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
+					<div class="col-md-5 p-lg-5 mx-auto my-5">
+						<h1 class="display-4 font-weight-normal">Chevron Edit</h1>
+						<p class="lead font-weight-normal">Editing</p>
+					</div>
+				</div>
+			</div>
+		</div>
+</div>
+
 <?php
 	include_once 'footer.php';
 ?>
