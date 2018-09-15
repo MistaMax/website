d528a4a8725b0f8f7833345a030c8bc10952b03f
Mista Max <mmctheace@gmail.com>
2018-09-15 15:28:05 -0500

    updating chevron edit

--- public_html/chevron/index.php
+++ public_html/chevron/index.php
@@ -7,7 +7,7 @@
                 <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
 					<div class="col-md-12 p-lg-5 mx-auto my-5">
 					<h1 class="display-4 font-weight-normal">Chevron Edit</h1>
-						<p class="lead font-weight-normal">Editing</p>
+						<p class="lead font-weight-normal">Currently forcing the signup page to have an error to prevent additional login creations</p>
 					</div>
 				</div>
 			</div>
