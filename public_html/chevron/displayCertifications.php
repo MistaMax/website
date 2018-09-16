3eae41d589144258de629f75b60b279beecb3f04
Mista Max <mmctheace@gmail.com>
2018-09-16 03:00:00 -0500

    updating chevron edit

--- public_html/chevron/displayCertifications.php
+++ public_html/chevron/displayCertifications.php
@@ -12,6 +12,7 @@
             <div class="col">
                 <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-semilight">
 					<div class="col-md-12 p-lg-5 mx-auto my-5">
+                    <h1 class="display-4 font-weight-normal">Display Certifications</h1>
                         <?php
                             $sql = "SELECT * FROM certifications WHERE 1";
                             $result = mysqli_query($connection, $sql);
